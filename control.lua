ec1d1e6e6611e0fcff08cb9443f15d2624d4b8fc
Kyle Brantley <kyle@averageurl.com>
2016-12-22 21:24:00 -0700

    Initial revision

---
+++ control.lua
@@ -0,0 +1,7 @@
+script.on_event(defines.events.on_resource_depleted, function(r)
+	resource.entity.amount = 100000
+end)
+
+script.on_event(defines.events.on_chunk_generated, function(c)
+	for _, e in pairs(c.surface.find_entities_filtered({area={chunk.area.left_topm chunk.area.right_bottom}, type="resource"})) do e.amount = 100000 end
+end)
