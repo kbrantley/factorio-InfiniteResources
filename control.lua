f6bc8a557afe95f8cbafeef53f7ba0e143101e39
Kyle Brantley <kyle@averageurl.com>
2016-12-22 21:57:15 -0700

    Bugfixes

--- control.lua
+++ control.lua
@@ -3,5 +3,5 @@ script.on_event(defines.events.on_resource_depleted, function(r)
 end)
 
 script.on_event(defines.events.on_chunk_generated, function(c)
-	for _, e in pairs(c.surface.find_entities_filtered({area={chunk.area.left_topm chunk.area.right_bottom}, type="resource"})) do e.amount = 100000 end
+	for _, e in pairs(c.surface.find_entities_filtered({area={c.area.left_top, c.area.right_bottom}, type="resource"})) do e.amount = 100000 end
 end)
